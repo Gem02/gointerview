4e5641637aaab584ce00b150d916a4447fa5a2c5
PC-GEM <godwinmangai3@gmail.com>
2025-03-10 11:00:54 -0700

    middleware

--- auth.ts
+++ auth.ts
@@ -3,7 +3,6 @@ import GoogleProvider from "next-auth/providers/google";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [
-    // Google OAuth
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
