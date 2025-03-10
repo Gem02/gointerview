bac4fea1f67a1c99ded01a6566929bb45900eb3e
PC-GEM <godwinmangai3@gmail.com>
2025-03-10 07:20:06 -0700

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -3,16 +3,16 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  const token = await getToken({ req, secret: process.env.AUTH_SECRET });
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET, raw: true });
 
+  // If no token, redirect to sign-in page
   if (!token) {
-    const signInUrl = new URL("/signin", req.url);
-    return NextResponse.redirect(signInUrl);
+    return NextResponse.redirect(new URL("/signin", req.url));
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/dashboard/:path*", "/profile", "/interview/:path*"], 
+  matcher: ["/dashboard/:path*", "/profile", "/interview/:path*"],
 };
