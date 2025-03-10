b77666f1649f3d0264207ff246e70f7a522d1800
PC-GEM <godwinmangai3@gmail.com>
2025-03-10 09:44:53 -0700

    routes edits

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,35 +1,4 @@
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import type { JWT } from "next-auth/jwt";
-import type { Session, User } from "next-auth";
+import { handlers } from "@/auth";
 
-const authOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
-  callbacks: {
-    async session({ session, token }: { session: Session; token: JWT }) {
-      if (session.user) {
-        session.user.id = token.sub ?? "";
-      }
-      return session;
-    },
-    async jwt({ token }: { token: JWT }) {
-      return token;
-    },
-  },
-  pages: {
-    signIn: "/signin",
-  },
-};
 
-export default NextAuth(authOptions);
-
-declare module "next-auth" {
-  interface Session {
-    user: User & { id: string };
-  }
-}
+export const {GET, POST} = handlers;
