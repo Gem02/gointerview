ef139fb223c06468c0f13f1eabb99b05ca646199
PC-GEM <godwinmangai3@gmail.com>
2025-03-13 05:03:16 -0700

    new auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,2 @@
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-
-const authOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
-  secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
-};
-
-const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST };
+import { handlers } from '@/auth/authSetup'
+export const { GET, POST } = handlers;
