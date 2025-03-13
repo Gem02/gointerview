e44e8a0e3cbe1da993e878d5bc8a045eccbb1d05
PC-GEM <godwinmangai3@gmail.com>
2025-03-13 05:32:50 -0700

    auth files

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,6 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useSession } from "next-auth/react";
 import { Menu, X } from "lucide-react";
-import { signIn } from "next-auth/react";
 
 function Navbar() {
   const path = usePathname();
@@ -87,7 +86,7 @@ function Navbar() {
                 )}
               </div>
             ) : (
-              <button className="bg-none text-sm font-semibold" onClick={() => signIn("google", {callbackUrl: "/dashboard"})}>Sign In</button>
+              <Link href={'/signin'}>Signin</Link>
             )}
           </div>
           
